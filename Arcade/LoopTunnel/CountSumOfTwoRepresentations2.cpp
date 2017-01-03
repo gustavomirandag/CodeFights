f93fcf624e1715382a661c6428da2774802979d1
gustavomirandag <gustavomirandag@gmail.com>
2017-01-03 10:59:18 -0200

    CountSumOfTwoRepresentations2 O(1) solution

--- Arcade/LoopTunnel/CountSumOfTwoRepresentations2.cpp
+++ Arcade/LoopTunnel/CountSumOfTwoRepresentations2.cpp
@@ -7,3 +7,11 @@ int countSumOfTwoRepresentations2(int n, int l, int r) {
 	}
 	return count;
 }
+
+//O(1)
+int countSumOfTwoRepresentations2(int n, int l, int r) {
+	if (2 * l > n) return 0;
+	if (2 * r < n) return 0;
+	l = std::max(l, n - r);
+	return n / 2 - l + 1;
+}
